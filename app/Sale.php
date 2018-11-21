97e20eba6d3d07e679e7fbab1c448db29c49b300
jivalenzuela <jivalenzuelaibarra@gmail.com>
2018-11-21 19:48:54 -0400

    solemne2

--- app/Sale.php
+++ app/Sale.php
@@ -3,11 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\SaleDetail;
 
 class Sale extends Model
 {
     //
-    public static function prueba(){
-return "prueba";
-    }
+    public static function saveSale(Sale $sale, array $products){
+		$sale->save();
+		$total_amount=0;
+		foreach ($products as $product) {
+		    $sale_detail = new SaleDetail();
+		    $sale_detail->sale_id = $sale->id;
+		    $sale_detail->product_code = $product->code;
+		    $sale_detail->product_detail = $product->name;
+		    $sale_detail->price = $product->price;
+		    $sale_detail->save();
+		    $total_amount+=$product->price;
+		}
+		$sale->total_amount=$total_amount;
+		$sale->save();
+	}
 }
